d08c72abdabcd806a1fb46b717c36852e519745b
PNG2228 <155178726+PNG2228@users.noreply.github.com>
2024-01-15 12:39:40 +0530

    Add files via upload

---
+++ TunaExample.java
@@ -0,0 +1,14 @@
+package javaprograms;
+
+public abstract class TunaExample {
+
+	public TunaExample() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int  
+	}
+
+}
